58af3bd958658f1790ae54153700825762f4d45a
Navaneeth Suresh <navaneeth22yt@gmail.com>
2022-12-17 19:12:18 +0530

    work pls 1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,20 +8,20 @@ import { navVariants } from '../utils/motion';
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} >
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white" >NAVANEETH</h2>
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">NAVANEETH</h2>
       <img
         src="/menu.svg"
         alt="menu"
         className="w-[24px] h-[24px] object-contain"
       />
     </div>
-    <div className='absolute w-[50%] inset-0 gradient-01' />
+    <div className="absolute w-[50%] inset-0 gradient-01" />
 
   </motion.nav>
 );
