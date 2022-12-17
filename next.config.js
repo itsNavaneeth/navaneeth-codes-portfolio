af4ccdf3ff744e101cef8544aac17fd61cae7bd9
Navaneeth Suresh <navaneeth22yt@gmail.com>
2022-12-17 19:57:27 +0530

    ughh

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
 };
 
-module.exports = {
-  nextConfig,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+module.exports = nextConfig;
