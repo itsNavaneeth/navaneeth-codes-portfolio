83d9e919afe242f4175bb29e2252bde5b737ebfa
Navaneeth Suresh <navaneeth22yt@gmail.com>
2022-12-17 18:49:02 +0530

    website loading commit

--- styles/index.js
+++ styles/index.js
@@ -19,7 +19,7 @@ const styles = {
     heroDText:
         'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
     heroTitle:
-        'font-bold lg:text-[100px] md:text-[60px] sm:text-[44px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] text-white',
+        'font-bold lg:text-[80px] md:text-[60px] sm:text-[44px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] text-white',
 };
 
 export default styles;
